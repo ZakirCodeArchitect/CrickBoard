aef5a44c9d20290db8ac6b8c288609beb8eb4365
Zair Matloob <zakirmatloob149@gmail.com>
2025-01-01 23:54:37 +0500

    refactoring

--- routes/scoreBoard.js
+++ routes/scoreBoard.js
@@ -3,8 +3,8 @@ const scoreBoardController = require("../controllers/scoreBoardController");
 
 const router = express.Router();
 
-router.get("/", scoreBoardController.scoreBoard);
-router.get("/store-score", scoreBoardController.storeScoreBoard);
-router.get("/info", scoreBoardController.getScoreBoard);
+    router.get("/", scoreBoardController.scoreBoard);
+    router.get("/store-score", scoreBoardController.storeScoreBoard);
+    router.get("/info", scoreBoardController.getScoreBoard);
 
 module.exports = router;
